371f6f6ecaf8db6d335a0211ff392f0257eda87c
Chitturi-NaveenKumar <chitturi.kumar1@gmail.com>
2023-03-01 12:52:22 +0530

    added

--- src/App.js
+++ src/App.js
@@ -1,35 +1,24 @@
 import React from "react";
-import { createHashRouter, Navigate } from "react-router-dom";
+import {
+  createBrowserRouter,
+  createHashRouter,
+  Navigate,
+} from "react-router-dom";
 import BackPage, { BackLoader, BacksLoader } from "./Pages/BackPage";
 import ErrorBackPage from "./Pages/ErrorBackPage";
 import ErrorPage from "./Pages/ErrorPage";
 import HomePage from "./Pages/HomePage";
 
-const App = createHashRouter([
-  {
-    path: "/",
-    element: <Navigate to="/home" />,
-  },
-  { path: "home", element: <HomePage /> },
-  {
-    path: "back",
-    element: <BackPage />,
+const App = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <Navigate to="/home" />,
+    },
+    { path: "/home", element: <HomePage /> },
 
-    errorElement: <ErrorPage />,
-    children: [
-      {
-        path: ":id",
-        loader: BackLoader,
-        element: <HomePage />,
-        errorElement: <ErrorBackPage />,
-      },
-      {
-        path: "me",
-        // loader: BacksLoader,
-        element: <BackPage />,
-      },
-    ],
-  },
-  { path: "*", element: <ErrorPage /> },
-]);
+    { path: "*", element: <ErrorPage /> },
+  ],
+  { basename: "/routerparcel" }
+);
 export default App;
