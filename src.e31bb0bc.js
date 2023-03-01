4710581044185651f5f217d753e8019cb47a5303
Chitturi-NaveenKumar <chitturi.kumar1@gmail.com>
2023-03-01 13:50:46 +0530

    Updates

--- src.e31bb0bc.js
+++ src.e31bb0bc.js
@@ -36029,7 +36029,7 @@ function _getRequireWildcardCache(nodeInterop) { if (typeof WeakMap !== "functio
 function _interopRequireWildcard(obj, nodeInterop) { if (!nodeInterop && obj && obj.__esModule) { return obj; } if (obj === null || typeof obj !== "object" && typeof obj !== "function") { return { default: obj }; } var cache = _getRequireWildcardCache(nodeInterop); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (key !== "default" && Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj.default = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 var App = (0, _reactRouterDom.createBrowserRouter)([{
-  path: "/",
+  path: "/routerparcel",
   element: /*#__PURE__*/_react.default.createElement(_reactRouterDom.Navigate, {
     to: "/home"
   })
@@ -36081,7 +36081,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52557" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52626" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
